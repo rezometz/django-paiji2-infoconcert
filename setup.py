e2564f0a89bbd0c1eeb3ccec4f532902d3213e17
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2015-08-19 15:23:23 +0200

    translation files autocompilation

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 import os
+import sys
 from setuptools import setup, find_packages
+from django.core import management
 
 with open(os.path.join(os.path.dirname(__file__), 'README.markdown')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+# translation files compilation
+currentdir = os.getcwd()
+os.chdir(os.path.join(currentdir, 'paiji2_infoconcert'))
+management.call_command('compilemessages', stdout=sys.stdout)
+os.chdir(currentdir)
 
 setup(
     name='django-paiji2-infoconcert',
